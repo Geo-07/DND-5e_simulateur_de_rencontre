cbc7b8aa20dded8151fce80d77d4be53d53fe0f9
Rémi TAUVEL <tauvel.remi@gmail.com>
2023-01-10 22:46:32 +0100

    ✨ find target

--- fight.py
+++ fight.py
@@ -1,4 +1,5 @@
 import dice
+from fighters import Fighter
 
 
 class Fight:
@@ -25,3 +26,16 @@ class Fight:
         """display fighters by initiative"""
         for index, fighter in enumerate(self.sorted_fighters):
             print(f"{fighter.nom}: initiative: {fighter.initiative} rang: {index+1}")
+
+    def find_first_character_of_team(self, team: str) -> Fighter:
+        """selects the fighter of the given team with the highest initiative
+
+        Args:
+            team (string): the team that is attacked
+
+        Returns:
+            Fighter: the fighter that is selected
+        """
+        for fighter in self.sorted_fighters:
+            if fighter.equipe == team:
+                return fighter
